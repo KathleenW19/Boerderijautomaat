bc1bf1cebca6938866db19b36975a8c8139d95c4
KathleenW19 <kathleen.wouters2001@gmail.com>
2025-05-30 14:17:20 +0200

    CSS opgeschoont

--- app/Models/Vak.php
+++ app/Models/Vak.php
@@ -20,18 +20,18 @@ class Vak extends Model
     }
 
     public function getAfbeeldingSrcAttribute()
-{
-    if ($this->status === 'vak geopend' && isset($this->product)) {
-        // Product afbeelding op de achtergrond en deur op de voorgrond
-        return asset($this->product->afbeelding_met_product); // De productafbeelding
-    } elseif ($this->status === 'leeg' || !isset($this->product)) {
-        // Als leeg, toon deur met lege status
-        return asset($this->product ? $this->product->deur_afbeelding : 'images/deur_dicht.png');
-    }
+    {
+        if ($this->status === 'vak geopend' && isset($this->product)) {
+            // Product afbeelding op de achtergrond en deur op de voorgrond
+            return asset($this->product->afbeelding_met_product); // De productafbeelding
+        } elseif ($this->status === 'leeg' || !isset($this->product)) {
+            // Als leeg, toon deur met lege status
+            return asset($this->product ? $this->product->deur_afbeelding : 'images/deur_dicht.png');
+        }
 
-    // Als het vak bezet is, toon deur met product zichtbaar erachter
-    return asset($this->product->afbeelding_met_product);
-}
+        // Als het vak bezet is, toon deur met product zichtbaar erachter
+        return asset($this->product->afbeelding_met_product);
+    }
 
     public function getAltTextAttribute()
     {
--- resources/views/components/vak.blade.php
+++ resources/views/components/vak.blade.php
@@ -6,13 +6,13 @@
     <div class="vak-image-container">
         @if ($vak->product)
             <img src="{{ asset($vak->product->afbeelding_met_product) }}"
-                class="card-img vak-product-afbeelding {{ $vak->status === 'leeg' ? 'verborgen' : '' }}"
+                class="card-img-automaat vak-product-afbeelding {{ $vak->status === 'leeg' ? 'verborgen' : '' }}"
                 data-vak-id="{{ $vak->id }}"
                 alt="Product afbeelding">
         @endif
 
             <img src="{{ asset('images/deur_dicht.png') }}"
-                class="card-img vak-deur-afbeelding"
+                class="card-img-automaat vak-deur-afbeelding"
                 data-vak-id="{{ $vak->id }}"
                 alt="Deur afbeelding">
     </div>
